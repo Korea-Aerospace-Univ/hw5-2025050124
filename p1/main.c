227690fbbfbdda7b025d944a3e4bf116cc3d48d9
2025050124 <ddd193923@gmail.com>
2026-04-29 16:11:58 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,46 +4,30 @@
 
 int main()
 {
-    int N; int i=1; int j=1; int k=1; int answer=0;
+    int N; int i=1; int j=2; int k=1; int answer=0;
     
     scanf("%d",&N);
     
     for(i=1; 900*i<=N;i++){
         
-        for(j=1; 1500*j<=N-900*i; j++){
+        for(j=2; 750*j<=N-900*i; j=j+2){
             
-            for(k=1; 200*k<=N-900*i-1500*j;k++){
+            for(k=1; 200*k<=N-900*i-750*j;k++){
                 
-                if(900*i+1500*j+200*k==N ){
+                if(900*i+750*j+200*k==N ){
                     
-                    if(i>k || 2*j>k){
-                        printf("%d %d %d\n",i,2*j,k);
+                    if(i>k || j>k){
+                        printf("%d %d %d\n",i,j,k);
                         
                         answer++;
                     }
                 }
+            }     
+        }
     }
-}
-
-
-
-
-
-
-
-
-}
-
-
-
-
-if(answer==0){
+   if(answer==0){
     printf("none");
-}
-
-
-
-
+   }
 
 
 }
