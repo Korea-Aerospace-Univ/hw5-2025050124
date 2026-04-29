edcbea311c030b6b9b212d8ac4f682731d21f0ac
2025050124 <ddd193923@gmail.com>
2026-04-29 16:22:58 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int N; int i=1; int j=2; int k=1; int answer=0;
+    int N; int i=1; int j=2; int k=1; bool found=false;
     
     scanf("%d",&N);
     
@@ -19,13 +19,13 @@ int main()
                     if(i>k || j>k){
                         printf("%d %d %d\n",i,j,k);
                         
-                        answer++;
+                        found=true;
                     }
                 }
             }     
         }
     }
-   if(answer==0){
+   if(!found){
     printf("none");
    }
 
